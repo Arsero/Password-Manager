e848302b73b795bb8d903aed8c7524cb1deb9300
Azzedine <54241944+Arsero@users.noreply.github.com>
2020-03-23 11:53:30 +0100

    Update NavBar.tsx

--- src/features/nav/NavBar.tsx
+++ src/features/nav/NavBar.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
 
 export const NavBar = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <Navbar bg="dark" variant="dark" fixed="top">
+      <Container>
+        <Navbar.Brand href="#home">Password Manager</Navbar.Brand>
+        <Button variant="warning">
+          <b>Add an account</b>
+        </Button>
+      </Container>
+    </Navbar>
+  );
+};

f21fb8f3e30005e93985fd980804038c7352db8b
Azzedine <54241944+Arsero@users.noreply.github.com>
2020-03-23 09:57:22 +0100

    Add Account List

---
+++ src/app/layout/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { AccountList } from '../../features/accounts/dashboard/AccountList'
+
+export const App = () => {
+    return (
+        <div>
+            <AccountList />
+        </div>
+    )
+}

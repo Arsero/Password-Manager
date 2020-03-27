0bf8db9e99efbaef3b2869844b10c54bb9e3398c
Arsero <54241944+Arsero@users.noreply.github.com>
2020-03-27 13:00:08 +0100

    Add account form

--- src/features/accounts/AccountForm.tsx
+++ src/features/accounts/AccountForm.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import { Form, Container, Button, ButtonGroup } from "react-bootstrap";
+import "./styles.css";
 
 export const AccountForm = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <Container>
+      <Form>
+        <Form.Group controlId="website">
+          <Form.Label>Website</Form.Label>
+          <Form.Control type="text" placeholder="Enter a website" />
+        </Form.Group>
+        <Form.Group controlId="email">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="name@example.com" />
+        </Form.Group>
+        <Form.Group controlId="username">
+          <Form.Label>Username</Form.Label>
+          <Form.Control type="text" placeholder="Enter a username" />
+        </Form.Group>
+        <Form.Group controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Enter a password" />
+        </Form.Group>
+        <Form.Group controlId="comment">
+          <Form.Label>Comment</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows="3"
+            placeholder="Any comment for your account ? Like a question."
+          />
+        </Form.Group>
+          <Button type="submit" variant="dark" style={{ margin: "25px auto" }} block>
+            Submit
+          </Button>
+          <Button variant="danger" block>Cancel</Button>
+      </Form>
+    </Container>
+  );
+};

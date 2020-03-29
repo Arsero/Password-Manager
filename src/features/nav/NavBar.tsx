dccd3fe45feb0cf36385d06d2154166522ad0d71
Arsero <54241944+Arsero@users.noreply.github.com>
2020-03-29 16:44:29 +0100

    Disable main title 'Password manager' link

--- src/features/nav/NavBar.tsx
+++ src/features/nav/NavBar.tsx
@@ -8,22 +8,26 @@ interface IProps {
   isLogged: boolean;
 }
 
-export const NavBar: React.FC<IProps> = ({isLogged}) => {
+export const NavBar: React.FC<IProps> = ({ isLogged }) => {
   const history = useHistory();
 
   return (
     <Navbar bg="dark" variant="dark" fixed="top">
       <Container>
-        <Navbar.Brand href="/">Password Manager</Navbar.Brand>
-        {isLogged && <Button variant="warning"
-          onClick={() => {
-            const location = {
-              pathname: '/createAccount'
-            }
-            history.push(location);
-          }}>
-          <b>Add an account</b>
-        </Button> }
+        <Navbar.Brand>Password Manager</Navbar.Brand>
+        {isLogged && (
+          <Button
+            variant="warning"
+            onClick={() => {
+              const location = {
+                pathname: "/createAccount"
+              };
+              history.push(location);
+            }}
+          >
+            <b>Add an account</b>
+          </Button>
+        )}
       </Container>
     </Navbar>
   );

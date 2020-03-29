b47394a37ec8efb000af2bef29ac05f4c84bcb97
Arsero <54241944+Arsero@users.noreply.github.com>
2020-03-29 16:43:21 +0100

    Add icon application
    Not working...

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { app, BrowserWindow } from 'electron';
 declare var MAIN_WINDOW_WEBPACK_ENTRY: any;
 // @ts-ignore
-import appIcon from "./assets/images/appIcon.png";
+import appIcon from "./assets/images/appIcon.ico";
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {

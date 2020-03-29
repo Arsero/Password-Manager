dfa5d2969240840072193c22c4f4d2c50de97f66
Azzedine <54241944+Arsero@users.noreply.github.com>
2020-03-29 23:48:22 +0200

    Refactor fileUtils (typescript)

--- src/utils/fileUtils.ts
+++ src/utils/fileUtils.ts
@@ -1,19 +1,18 @@
 import fs from "fs";
 
 export default class FileUtils {
-  static saveData = (path, data) => {
-    const jsonData = JSON.stringify(data);
-    fs.writeFileSync(path, jsonData, "utf-8");
+  static saveData = (path: string, data: string) => {
+    fs.writeFileSync(path, data, "utf-8");
     console.log(path + " saved !");
   };
   
-  static loadData = path => {
+  static loadData = (path: string) => {
     let data = fs.readFileSync(path, "utf-8");
     console.log(path + " loaded !");
     return data;
   };
   
-  static exists = path => {
+  static exists = (path: string) => {
     return fs.existsSync(path);
   }
 }

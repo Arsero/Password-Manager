1ac2b545871f87cc55c997f242312d4cb0a8dcad
Azzedine <54241944+Arsero@users.noreply.github.com>
2020-04-26 19:04:17 +0200

    Remove the menu bar

--- src/main.ts
+++ src/main.ts
@@ -25,6 +25,7 @@ const createWindow = () => {
   // and load the index.html of the app.
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
+  mainWindow.setMenuBarVisibility(false);
   // Open the DevTools.
   //mainWindow.webContents.openDevTools();
 

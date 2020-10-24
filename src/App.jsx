97981934e3868661593903ab485282ced75649b0
Azzedine <54241944+Arsero@users.noreply.github.com>
2020-10-24 15:32:00 +0200

    remove bulma

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { hot } from 'react-hot-loader';
 
-const App = () => <div>Hi from react !</div>;
+const App = () => <div className='container'>Hi from react !</div>;
 
 export default hot(module)(App);

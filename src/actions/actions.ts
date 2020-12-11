cbe452853b887ba083d73d492a4b00021b4d6ec3
Azzedine <54241944+Arsero@users.noreply.github.com>
2020-12-11 17:01:33 +0100

    Add action

---
+++ src/actions/actions.ts
@@ -0,0 +1,7 @@
+export const LOGIN = 'LOGIN';
+export const REGISTER = 'REGISTER';
+export const ADD_ACCOUNT = 'ADD_ACCOUNT';
+export const UPD_ACCOUNT = 'UPD_ACCOUNT';
+export const DEL_ACCOUNT = 'DEL_ACCOUNT';
+export const SELECT_ACCOUNT = 'SELECT_ACCOUNT';
+export const GET_ACCOUNT = 'GET_ACCOUNT';

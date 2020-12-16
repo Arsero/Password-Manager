731372ae678f0fda7a3d500d35b01667b42bed9e
Azzedine <54241944+Arsero@users.noreply.github.com>
2020-12-16 14:38:01 +0100

    Update action

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -1,3 +1,5 @@
+import Account from '../models/account';
+
 export const LOGIN = 'LOGIN';
 export const REGISTER = 'REGISTER';
 export const ADD_ACCOUNT = 'ADD_ACCOUNT';
@@ -5,3 +7,32 @@ export const UPD_ACCOUNT = 'UPD_ACCOUNT';
 export const DEL_ACCOUNT = 'DEL_ACCOUNT';
 export const SELECT_ACCOUNT = 'SELECT_ACCOUNT';
 export const COPY_PWD = 'COPY_PWD';
+
+export const Login = (secret: string) => ({ type: LOGIN, payload: secret });
+
+export const Register = (secret: string) => ({
+  type: REGISTER,
+  payload: secret,
+});
+
+export const AddAccount = (account: Account) => ({
+  type: ADD_ACCOUNT,
+  payload: account,
+});
+
+export const UpdateAccount = (account: Account) => ({
+  type: UPD_ACCOUNT,
+  payload: account,
+});
+
+export const DeleteAccount = (id: string) => ({
+  type: DEL_ACCOUNT,
+  payload: id,
+});
+
+export const SetSelectedAccount = (account: Account) => ({
+  type: SELECT_ACCOUNT,
+  payload: account,
+});
+
+export const CopyPassword = (id: string) => ({ type: COPY_PWD, payload: id });

ffffeb020b3e3c19a2d4c99d49062998480bb04a
Azzedine <54241944+Arsero@users.noreply.github.com>
2021-02-19 17:12:09 +0100

    Fix menu margin top

--- src/components/menu/Sidebar.tsx
+++ src/components/menu/Sidebar.tsx
@@ -3,44 +3,44 @@ import { useHistory } from 'react-router-dom';
 import { Container, Button, Form, Row } from 'react-bootstrap';
 
 export const Sidebar = ({ Close }: any) => {
-  const history = useHistory();
+	const history = useHistory();
 
-  return (
-    <Container style={{ width: '50%', marginTop: '90%' }}>
-      <Form>
-        <Row>
-          <Button
-            variant='outline-light'
-            style={{ marginBottom: '30px', width: '100%' }}
-            onClick={(event) => {
-              Close();
-              event.preventDefault();
-              const location = {
-                pathname: '/account',
-              };
-              history.push(location);
-            }}
-          >
-            Add an account
-          </Button>
-        </Row>
-        <Row>
-          <Button
-            variant='outline-light'
-            style={{ width: '100%' }}
-            onClick={(event) => {
-              Close();
-              event.preventDefault();
-              const location = {
-                pathname: '/secret',
-              };
-              history.push(location);
-            }}
-          >
-            Update secret
-          </Button>
-        </Row>
-      </Form>
-    </Container>
-  );
+	return (
+		<Container style={{ width: '50%', marginTop: '90%' }}>
+			<Form>
+				<Row>
+					<Button
+						variant='outline-light'
+						style={{ marginBottom: '30px', width: '100%' }}
+						onClick={(event) => {
+							Close();
+							event.preventDefault();
+							const location = {
+								pathname: '/account',
+							};
+							history.push(location);
+						}}
+					>
+						Add an account
+					</Button>
+				</Row>
+				<Row>
+					<Button
+						variant='outline-light'
+						style={{ width: '100%' }}
+						onClick={(event) => {
+							Close();
+							event.preventDefault();
+							const location = {
+								pathname: '/secret',
+							};
+							history.push(location);
+						}}
+					>
+						Update secret
+					</Button>
+				</Row>
+			</Form>
+		</Container>
+	);
 };
--- src/containers/menu/Menu.tsx
+++ src/containers/menu/Menu.tsx
@@ -5,65 +5,62 @@ import { Button } from 'react-bootstrap';
 import { List } from 'react-bootstrap-icons';
 
 export const Menu = () => {
-  const [isOpen, setIsOpen] = useState(false);
+	const [isOpen, setIsOpen] = useState(false);
 
-  const Close = () => {
-    setIsOpen(false);
-  };
+	const Close = () => {
+		setIsOpen(false);
+	};
 
-  return (
-    <div>
-      <Button
-        variant='outline-dark'
-        id='menu-button'
-        aria-label='Menu'
-        aria-controls='menu'
-        aria-expanded={isOpen}
-        onClick={() => setIsOpen(true)}
-        size='sm'
-        style={{
-          float: 'left',
-          marginTop: '-70px',
-          marginLeft: '12px',
-        }}
-      >
-        <List size={16} />
-      </Button>
-      <OffCanvas
-        isOpen={isOpen}
-        onClose={() => setIsOpen(false)}
-        labelledby='menu-button'
-        width='350px'
-        height='100%'
-        style={{
-          overlay: {
-            top: '28px',
-          },
-          content: {
-            background: 'rgba(5, 5, 5, 0.85)',
-          },
-        }}
-      >
-        <Button
-          variant='outline-light'
-          id='menu-button'
-          aria-label='Menu'
-          aria-controls='menu'
-          aria-expanded={isOpen}
-          onClick={() => setIsOpen(false)}
-          size='sm'
-          style={{
-            float: 'right',
-            marginTop: '12px',
-            marginRight: '12px',
-            paddingBottom: '3px',
-            paddingTop: 0,
-          }}
-        >
-          <b>x</b>
-        </Button>
-        <Sidebar Close={Close} />
-      </OffCanvas>
-    </div>
-  );
+	return (
+		<div>
+			<Button
+				variant='outline-dark'
+				id='menu-button'
+				aria-label='Menu'
+				aria-controls='menu'
+				aria-expanded={isOpen}
+				onClick={() => setIsOpen(true)}
+				size='sm'
+				style={{
+					float: 'left',
+					marginTop: '-70px',
+					marginLeft: '12px',
+				}}
+			>
+				<List size={16} />
+			</Button>
+			<OffCanvas
+				isOpen={isOpen}
+				onClose={() => setIsOpen(false)}
+				labelledby='menu-button'
+				width='350px'
+				height='100%'
+				style={{
+					content: {
+						background: 'rgba(5, 5, 5, 0.85)',
+					},
+				}}
+			>
+				<Button
+					variant='outline-light'
+					id='menu-button'
+					aria-label='Menu'
+					aria-controls='menu'
+					aria-expanded={isOpen}
+					onClick={() => setIsOpen(false)}
+					size='sm'
+					style={{
+						float: 'right',
+						marginTop: '12px',
+						marginRight: '12px',
+						paddingBottom: '3px',
+						paddingTop: 0,
+					}}
+				>
+					<b>x</b>
+				</Button>
+				<Sidebar Close={Close} />
+			</OffCanvas>
+		</div>
+	);
 };
